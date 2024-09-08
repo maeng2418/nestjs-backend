cb2b6192d0e1547c3a3c0220ef63a6a1c2a7c8c9
김명성 <maeng2418@naver.com>
2024-09-08 12:55:36 +0900

    [feat] 유효성 검사 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import ValidationPipe from './users/pipe/validation.pipe';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,21 +1,35 @@
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
 import {
   IsEmail,
   IsString,
-  IsStrongPassword,
+  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
 
 class CreateUserDto {
+  @Transform(({ value, obj }) => value.trim())
   @IsString()
-  @MinLength(1)
-  @MaxLength(20)
-  name: string;
+  @MinLength(2)
+  @MaxLength(30)
+  readonly name: string;
 
+  @IsString()
   @IsEmail()
+  @MaxLength(60)
   email: string;
 
-  @IsStrongPassword()
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(obj.name.trim())) {
+      throw new BadRequestException(
+        'password는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   password: string;
 }
 
