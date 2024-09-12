5bfc38db1b4c40f4931773092a058dc9821bc33c
김명성 <maeng2418@naver.com>
2024-09-13 00:55:00 +0900

    [feat] 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,26 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: [`${__dirname}/config/env/${process.env.NODE_ENV}.env`],
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       load: [emailConfig],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
     }),
   ],
   controllers: [AppController],
