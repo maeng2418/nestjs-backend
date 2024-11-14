fdebb1cf37e709520a3560cfe3726a432a75e60a
김명성 <maeng2418@naver.com>
2024-11-15 00:14:32 +0900

    [feat] 내장 로거

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name); // 어느 콘텍스트에서 로깅을 하는지 알려주는 이름을 제공
+
   getHello(): string {
-    return process.env.DATABASE_HOST;
+    this.logger.error('level: error');
+    this.logger.warn('level: warn');
+    this.logger.log('level: log');
+    this.logger.verbose('level: verbose');
+    this.logger.debug('level: debug');
+
+    return 'Hello World!';
   }
 }
