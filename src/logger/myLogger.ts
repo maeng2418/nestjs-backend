1e00585e5557c84f5cd3791590e79d8ae6f2739e
김명성 <maeng2418@naver.com>
2024-11-16 01:01:27 +0900

    [feat] 커스텀 로그 고도화

--- src/logger/myLogger.ts
+++ src/logger/myLogger.ts
@@ -1,24 +1,35 @@
-import { LoggerService } from '@nestjs/common';
+/* eslint-disable prefer-rest-params */
+import { ConsoleLogger } from '@nestjs/common';
 
-class MyLogger implements LoggerService {
-  log(message: string, ...optionalParmas: any[]): void {
-    console.log(message);
+class MyLogger extends ConsoleLogger {
+  error(message: any, stack?: string, context?: string) {
+    super.error.apply(this, arguments);
+    this.doSomething();
   }
 
-  error(message: string, ...optionalParmas: any[]): void {
-    console.log(message);
+  warn(message: any, stack?: string, context?: string) {
+    super.warn.apply(this, arguments);
+    this.doSomething();
   }
 
-  warn(message: string, ...optionalParmas: any[]): void {
-    console.log(message);
+  log(message: any, stack?: string, context?: string) {
+    super.log.apply(this, arguments);
+    this.doSomething();
   }
 
-  debug?(message: string, ...optionalParmas: any[]): void {
-    console.log(message);
+  verbose(message: any, stack?: string, context?: string) {
+    super.verbose.apply(this, arguments);
+    this.doSomething();
   }
 
-  verbose?(message: string, ...optionalParmas: any[]): void {
-    console.log(message);
+  debug(message: any, stack?: string, context?: string) {
+    super.debug.apply(this, arguments);
+    this.doSomething();
+  }
+
+  private doSomething() {
+    // 여기에 로깅에 관련된 부가 로직을 추가합니다.
+    // ex. DB에 저장
   }
 }
 
