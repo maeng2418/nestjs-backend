c882ef7d9f24a14a097d71f463186f15e8cf4a4f
김명성 <maeng2418@naver.com>
2024-11-25 00:40:01 +0900

    [feat] DB 헬스 체크

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -3,6 +3,7 @@ import {
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
+  TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
 
 @Controller('health-check')
@@ -10,16 +11,19 @@ export class HealthCheckController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    // pingCheck 함수를 이용하여 서비스가 제공하는 다른 서버가 잘 동작하고 있는지 확인한다.
-    // 위 예에서는 https://docs.nestjs.com 에 요청을 보내고
-    // 응답을 잘 받으면 응답 결과에 첫 번째 인수로 넣은 nestjs-docs로 응답
     return this.health.check([
+      // pingCheck 함수를 이용하여 서비스가 제공하는 다른 서버가 잘 동작하고 있는지 확인한다.
+      // https://docs.nestjs.com 에 요청을 보내고
+      // 응답을 잘 받으면 응답 결과에 첫 번째 인수로 넣은 nestjs-docs로 응답
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      // DB 헬스 체크
+      () => this.db.pingCheck('database'),
     ]);
   }
 }
